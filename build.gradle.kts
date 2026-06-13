fcfad3d3c716a9ad74d887fc6556114625996fbf
EYoka25 <165523650+EYoka25@users.noreply.github.com>
2026-06-13 19:39:06 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
